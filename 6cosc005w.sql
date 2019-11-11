04d3e5cd351022bd07adead80c47486c50f7ed7f
Sangeeth Karunaratne <sangeethdkarunaratne@gmail.com>
2019-11-11 11:32:43 +0530

    Updated SQL

--- 6cosc005w.sql
+++ 6cosc005w.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 30, 2019 at 07:38 PM
+-- Generation Time: Nov 11, 2019 at 06:58 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.1.30
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `6cosc005w`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
+('ebe46a59d1a67a2373dfcd74068ade366641756b', '::1', 1573449548, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537333434393534383b),
+('5b7b52810eda4a4609f65d5f6924c6a12e94a589', '::1', 1573450448, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537333435303434383b),
+('d507ab039d5e437f12bde8f8cbae800c45104c6a', '::1', 1573450793, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537333435303739333b73657373696f6e446174617c613a323a7b733a383a22757365726e616d65223b733a32323a2273616e67656574683134336b40676d61696c2e636f6d223b733a393a226c6f676765645f696e223b623a313b7d),
+('0202da3a2a286cae4320f9456a83a41e58a3d4a7', '::1', 1573451093, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537333435303739333b73657373696f6e446174617c613a333a7b733a393a22757365725f6e616d65223b733a31393a2273616e67656574687340676d61696c2e636f6d223b733a393a226c6f676765645f696e223b623a313b733a31303a2273657373696f6e5f6964223b733a34303a2230323032646133613261323836636165343332306639343536613833613431653538613364346137223b7d),
+('f7c23f55071d8bdeaf3fc4ad98b9bea0bfef3f1f', '::1', 1573451637, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537333435313633373b73657373696f6e446174617c613a333a7b733a393a22757365725f6e616d65223b733a32363a2273616e6765657468317373737334336b40676d61696c2e636f6d223b733a393a226c6f676765645f696e223b623a313b733a31303a2273657373696f6e5f6964223b733a34303a2266376332336635353037316438626465616633666334616439386239626561306266656633663166223b7d),
+('3cd2aea6fba28a16a2715bfce6e6891a264513a7', '::1', 1573451790, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537333435313633373b);
 
 -- --------------------------------------------------------
 
@@ -52,6 +77,29 @@ INSERT INTO `Movies` (`Id`, `Title`, `Director`, `Genre`, `IMDBrating`, `Release
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ToDoActions`
+--
+
+CREATE TABLE `ToDoActions` (
+  `Id` int(11) NOT NULL,
+  `UserId` varchar(255) NOT NULL,
+  `ActionTitle` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ToDoActions`
+--
+
+INSERT INTO `ToDoActions` (`Id`, `UserId`, `ActionTitle`) VALUES
+(6, 'UserId01', 'Complete Tutorial 01'),
+(7, 'UserId02', 'Complete Tutorial 02'),
+(8, 'UserId03', 'Complete Tutorial 03'),
+(9, 'UserId04', 'Complete Tutorial 04'),
+(10, 'UserId05', 'Complete Tutorial 05');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `todoActions`
 --
 -- Error reading structure for table 6cosc005w.todoActions: #1932 - Table '6cosc005w.todoActions' doesn't exist in engine
@@ -62,9 +110,21 @@ INSERT INTO `Movies` (`Id`, `Title`, `Director`, `Genre`, `IMDBrating`, `Release
 --
 
 --
+-- Indexes for table `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD KEY `ci_sessions_timestamp` (`timestamp`);
+
+--
 -- Indexes for table `Movies`
 --
 ALTER TABLE `Movies`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `ToDoActions`
+--
+ALTER TABLE `ToDoActions`
   ADD PRIMARY KEY (`Id`);
 
 --
@@ -76,6 +136,12 @@ ALTER TABLE `Movies`
 --
 ALTER TABLE `Movies`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `ToDoActions`
+--
+ALTER TABLE `ToDoActions`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
